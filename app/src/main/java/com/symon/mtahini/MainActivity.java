11518f8ca37cb0397dfc37dac2596f38105af655
SymonMuchemi <simonkenya846@gmail.com>
2023-10-24 22:38:44 +0300

    feat: login activity

--- app/src/main/java/com/symon/mtahini/MainActivity.java
+++ app/src/main/java/com/symon/mtahini/MainActivity.java
@@ -2,13 +2,25 @@ package com.symon.mtahini;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.login_text_view);
+        textView.setOnClickListener(
+                v -> {
+                    Intent intent = new Intent(this, Login.class);
+                    startActivity(intent);
+                }
+        );
+
     }
 }
