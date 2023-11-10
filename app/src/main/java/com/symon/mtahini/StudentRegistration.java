42637e6acd48dc0cb50e6a5a7dc0c713a78baabd
SymonMuchemi <simonkenya846@gmail.com>
2023-11-11 01:35:54 +0300

    feat: link to db

--- app/src/main/java/com/symon/mtahini/StudentRegistration.java
+++ app/src/main/java/com/symon/mtahini/StudentRegistration.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -12,22 +13,25 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 
-// this is in a new branch
 public class StudentRegistration extends AppCompatActivity {
 
-    EditText name_input, email_input, password_input, confirm_password_input;
+    EditText name_input, email_input,reg_number_input, password_input, confirm_password_input;
     ImageButton imageButton;
     Button register_button;
     TextView loginTextView;
     Navigation appNavigation;
-
-    String name, email, password, confirm_password;
+    String name, email, regNumber, password, confirm_password;
     String regexPattern = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
 
+    FirebaseDatabase database;
+    DatabaseReference reference;
     protected FirebaseAuth mAuth;
 
     @Override
@@ -51,6 +55,7 @@ public class StudentRegistration extends AppCompatActivity {
         imageButton = findViewById(R.id.stdRegArrowButton);
         loginTextView = findViewById(R.id.login_text_view);
         name_input = findViewById(R.id.student_name_edit_text);
+        reg_number_input = findViewById(R.id.reg_number_edit_text);
         email_input = findViewById(R.id.email_edit_text);
         password_input = findViewById(R.id.password_edit_text);
         confirm_password_input = findViewById(R.id.confirm_password_edit_text);
@@ -59,33 +64,16 @@ public class StudentRegistration extends AppCompatActivity {
         // handle user registration
         register_button.setOnClickListener(v -> {
             name = String.valueOf(name_input.getText());
+            regNumber = String.valueOf(reg_number_input.getText());
             email = String.valueOf(email_input.getText());
             password = String.valueOf(password_input.getText());
             confirm_password = String.valueOf(confirm_password_input.getText());
 
-            if (name.isEmpty()){
-                name_input.setError("This field cannot be empty!");
-                name_input.requestFocus();
-                name_input.setBackgroundResource(R.drawable.alert_bg);
-                return;
-            }
-            if (email.isEmpty()){
-                email_input.setError("This field cannot be empty!");
-                email_input.requestFocus();
-                email_input.setBackgroundResource(R.drawable.alert_bg);
-                return;
-            }
-            if (password.isEmpty()){
-                password_input.setError("This field cannot be empty!");
-                password_input.requestFocus();
-                password_input.setBackgroundResource(R.drawable.alert_bg);
-                return;
-            }
-            if (confirm_password.isEmpty()){
-                confirm_password_input.setError("This field cannot be empty!");
-                confirm_password_input.requestFocus();
-                confirm_password_input.setBackgroundResource(R.drawable.alert_bg);
-            }
+            if (!(checkStringInput(name_input, name))) return;
+            if (!(checkStringInput(reg_number_input, regNumber))) return;
+            if (!(checkStringInput(email_input, email))) return;
+            if (!(checkStringInput(password_input, password))) return;
+            if (!(checkStringInput(confirm_password_input, confirm_password))) return;
 
             if (!(confirm_password.equals(password))){
                 confirm_password_input.setError("Password mismatch!");
@@ -112,7 +100,16 @@ public class StudentRegistration extends AppCompatActivity {
                         } else {
                             Toast.makeText(StudentRegistration.this, "Unable to create account", Toast.LENGTH_SHORT).show();
                         }
-                    });
+                    }).addOnFailureListener( e -> Log.d("FB_AUTH", Objects.requireNonNull(e.getLocalizedMessage())));
+
+            Student student = new Student(name, email, regNumber);
+            database = FirebaseDatabase.getInstance();
+            reference = database.getReference("Users");
+            reference.child(regNumber).setValue(student).addOnCompleteListener(
+                    task -> Toast.makeText(StudentRegistration.this, "Details recorded successfully!", Toast.LENGTH_SHORT).show())
+                    .addOnFailureListener(
+                    e -> Log.d("DB", Objects.requireNonNull(e.getLocalizedMessage()))
+            );
         });
 
         // go to main activity on click
@@ -130,5 +127,15 @@ public class StudentRegistration extends AppCompatActivity {
                     startActivity(loginPage);
                 }
         );
+    }
+
+    public boolean checkStringInput(EditText editText, String text){
+        if (text.isEmpty()){
+            editText.setError("This part cannot be empty");
+            editText.requestFocus();
+            editText.setBackgroundResource(R.drawable.alert_bg);
+            return false;
+        }
+        return true;
     }
 }
