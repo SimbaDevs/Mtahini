c8ecb8ed2332b3708004b2fd15ad83e0f2b2cef2
SymonMuchemi <simonkenya846@gmail.com>
2023-11-18 01:26:15 +0300

    feat: enterCatMarks activity done page complete

--- app/src/main/java/com/symon/mtahini/LecturerLanding.java
+++ app/src/main/java/com/symon/mtahini/LecturerLanding.java
@@ -3,12 +3,22 @@ package com.symon.mtahini;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LecturerLanding extends AppCompatActivity {
 
+    Navigation appNav = new Navigation(this, LecturerLanding.this);
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lecturer_landing);
+
+        button = findViewById(R.id.lec_assignment_1_button);
+        button.setOnClickListener(
+                v -> {
+                    appNav.moveTo(CatMarksInput.class);
+                }
+        );
     }
 }
